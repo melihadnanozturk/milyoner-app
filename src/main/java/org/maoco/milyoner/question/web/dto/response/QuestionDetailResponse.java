e7d5a965a753a28f644f218c490d930d57c99558
M.Adnan <adnanmelihozturk@gmail.com>
2026-01-01 16:52:16 +0300

    getter added

--- src/main/java/org/maoco/milyoner/question/web/dto/response/QuestionDetailResponse.java
+++ src/main/java/org/maoco/milyoner/question/web/dto/response/QuestionDetailResponse.java
@@ -1,10 +1,12 @@
 package org.maoco.milyoner.question.web.dto.response;
 
+import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @SuperBuilder
+@Getter
 public class QuestionDetailResponse extends QuestionResponse {
     private List<AnswerResponse> answers;
 }
