e12b1cb5e1beb8c8c5244b69523b35f82e3a354c
M.Adnan <adnanmelihozturk@gmail.com>
2026-02-23 17:25:52 +0300

    some edited

--- src/main/java/org/maoco/milyoner/gameplay/data/repository/GameRepository.java
+++ src/main/java/org/maoco/milyoner/gameplay/data/repository/GameRepository.java
@@ -1,13 +1,13 @@
 package org.maoco.milyoner.gameplay.data.repository;
 
 import org.maoco.milyoner.gameplay.data.entity.GameEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface GameRepository extends CrudRepository<GameEntity, String> {
+public interface GameRepository extends JpaRepository<GameEntity, String> {
     Optional<GameEntity> findByUsername(String username);
 
     Optional<GameEntity> findByUsernameAndId(String username, String id);
